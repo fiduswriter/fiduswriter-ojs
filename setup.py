b70c40e493e08c13ce945ef6c8d45ffc928e7f37
Johannes Wilm <mail@johanneswilm.org>
2020-03-03 01:42:40 +0100

    3.8.0rc1

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='fiduswriter-ojs',
-    version='3.7.2',
+    version='3.8.0rc1',
     packages=find_namespace_packages(),
     include_package_data=True,
     exclude_package_data={
