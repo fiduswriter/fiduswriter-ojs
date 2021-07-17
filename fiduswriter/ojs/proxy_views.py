764367ccf0df91339d06f75c2f785a51b864a302
Johannes Wilm <mail@johanneswilm.org>
2021-07-17 15:55:35 +0200

    adjust queries/tests

--- fiduswriter/ojs/proxy_views.py
+++ fiduswriter/ojs/proxy_views.py
@@ -233,7 +233,7 @@ class Proxy(DjangoHandlerMixin, RequestHandler):
         # submission was successful, so we replace the user's write access
         # rights with read rights.
         right = AccessRight.objects.get(
-            holder_obj=self.user,
+            user=self.user,
             document=self.revision.document
         )
         right.rights = 'read'
